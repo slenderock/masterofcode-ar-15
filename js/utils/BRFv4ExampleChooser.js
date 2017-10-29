f4a9776261eb65d2086f2a3e0f6f0ee781cc433e
Serhii Ovcharenko <serhii.ovcharenko.work@gmail.com>
2017-10-29 03:00:34 +0200

    remove useless stuf from select

--- js/utils/BRFv4ExampleChooser.js
+++ js/utils/BRFv4ExampleChooser.js
@@ -5,39 +5,18 @@
 
   var urlMap = {
 
-    "+++ basic - face detection +++": null,
+    "-------": null,
 
-    "basic - face detection - detect in whole image": "js/examples/face_detection/detect_in_whole_image.js",
-    "basic - face detection - detect in center": "js/examples/face_detection/detect_in_center.js",
-    "basic - face detection - detect smaller faces": "js/examples/face_detection/detect_smaller_faces.js",
-    "basic - face detection - detect larger faces": "js/examples/face_detection/detect_larger_faces.js",
-
-    "+++ basic - face tracking +++": null,
+    "+++ Scheme +++": null,
 
     "basic - face tracking - track single face": "js/examples/face_tracking/track_single_face.js",
-    "basic - face tracking - track multiple faces": "js/examples/face_tracking/track_multiple_faces.js",
     "basic - face tracking - candide overlay": "js/examples/face_tracking/candide_overlay.js",
 
-    "+++ basic - point tracking +++": null,
-
-    "basic - point tracking - track multiple points": "js/examples/point_tracking/track_multiple_points.js",
-    "basic - point tracking - track points and face": "js/examples/point_tracking/track_points_and_face.js",
-
-    "+++ intermediate - face tracking +++": null,
-
-    "intermediate - face tracking - restrict to center": "js/examples/face_tracking/restrict_to_center.js",
-    "intermediate - face tracking - extended face": ["js/utils/BRFv4ExtendedFace.js", "js/examples/face_tracking/extended_face_shape.js"],
-    "intermediate - face tracking - smile detection": "js/examples/face_tracking/smile_detection.js",
-    "intermediate - face tracking - yawn detection": "js/examples/face_tracking/yawn_detection.js",
-    "intermediate - face tracking - png/mask overlay": "js/examples/face_tracking/png_mask_overlay.js",
-    "intermediate - face tracking - color libs": "js/examples/face_tracking/color_libs.js",
-
-    "+++ advanced - face tracking +++": null,
-
-    "advanced - face tracking - blink detection": "js/examples/face_tracking/blink_detection.js",
-    "advanced - face tracking - ThreeJS example": "js/examples/face_tracking/ThreeJS_example.js",
+    "+++ Mascs +++": null,
     "advanced - face tracking - face texture overlay": ["assets/brfv4_face_textures.js", "js/examples/face_tracking/face_texture_overlay.js"],
-    "advanced - face tracking - face swap (two faces)": "js/examples/face_tracking/face_swap_two_faces.js"
+
+    "--------": null
+
   };
   var labels = [];
   for (var key in urlMap) {
